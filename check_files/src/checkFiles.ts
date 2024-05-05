c957da8173439278bcbcae9f9eb0f7ad2ecb32c1
Lucas Pereira <pereirasantana@yahoo.com.br>
2024-05-05 18:29:36 -0300

    gfgfdhfghjfjj

--- check_files/src/checkFiles.ts
+++ check_files/src/checkFiles.ts
@@ -1,10 +1,14 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
 import * as fs from 'fs';
+import * as path from 'path';
 
 async function run() {
   try {
-    const requiredFiles = ['Dockerfile', 'fly.toml'];
+    // Definindo o diretório base como dois níveis acima do diretório atual
+    const baseDir = path.resolve(__dirname, '../../');
+
+    const requiredFiles = ['Dockerfile', 'fly.toml'].map(file => path.join(baseDir, file));
 
     requiredFiles.forEach(file => {
       if (!fs.existsSync(file)) {
